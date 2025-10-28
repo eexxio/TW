606c309d8eef9dabea840e2dcb221423f8bfa3d9
eduard <93722405+eexxio@users.noreply.github.com>
2025-10-28 17:57:06 +0200

    Create base entity and Movie entity

---
+++ cinema-movies-service/src/main/java/com/cinema/movies/entity/BaseEntity.java
@@ -0,0 +1,46 @@
+package com.cinema.movies.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Data
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ cinema-movies-service/src/main/java/com/cinema/movies/entity/Movie.java
@@ -0,0 +1,55 @@
+package com.cinema.movies.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "movies")
+public class Movie extends BaseEntity {
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(length = 100)
+    private String genre;
+
+    @Column
+    private Integer duration;
+
+    @Column(length = 255)
+    private String director;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    @Column(precision = 3, scale = 1)
+    private Double rating;
+
+    @Column(name = "poster_url", length = 500)
+    private String posterUrl;
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + getId() +
+                ", title='" + title + '\'' +
+                ", genre='" + genre + '\'' +
+                ", director='" + director + '\'' +
+                ", releaseDate=" + releaseDate +
+                ", rating=" + rating +
+                ", duration=" + duration +
+                '}';
+    }
+}
