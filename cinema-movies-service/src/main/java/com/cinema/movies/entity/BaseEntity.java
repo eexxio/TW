3e6499624a84ebac73678dccad0b034d00b4b439
eduard <93722405+eexxio@users.noreply.github.com>
2025-10-29 21:44:39 +0200

    Fix data annotations

--- cinema-movies-service/src/main/java/com/cinema/movies/entity/BaseEntity.java
+++ cinema-movies-service/src/main/java/com/cinema/movies/entity/BaseEntity.java
@@ -1,12 +1,14 @@
 package com.cinema.movies.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-@Data
+@Getter
+@Setter
 @MappedSuperclass
 public abstract class BaseEntity {
 
