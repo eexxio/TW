c2eb916236fc629e010279c42cc700f7ac8d2e65
tesha <tesh.alexandru@gmail.com>
2025-10-31 11:42:17 +0200

    Add User entity

---
+++ cinema-users-service/src/main/java/entity/User.java
@@ -0,0 +1,105 @@
+package entity;
+
+import enums.Role;
+import jakarta.persistence.*;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Table(name="users")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long user_id;
+
+    @Column(name = "first_name")
+    private String firstname;
+
+    @Column(name = "last_name")
+    private String lastname;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "password_hash")
+    private String password;
+
+    @Column(name ="phone_number")
+    private String phoneNumber;
+
+    @Column(name="date_of_birth")
+    private String dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name="updated_at")
+    private LocalDateTime updatedAt;
+
+    public User() {
+
+    }
+
+    public User(Long user_id, String firstname, String lastname,
+                String email, String password, String phoneNumber,
+                String dateOfBirth, Role role, LocalDateTime createdAt,
+                LocalDateTime updatedAt) {
+        this.user_id = user_id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.role = role;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
