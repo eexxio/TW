324bc6557ef5656a2ffaea869b488b60a13df72f
Alexandru Teșulă <alexandru.tesula@student.unitbv.ro>
2026-01-11 22:17:58 +0200

    Resolve deprecation warning

--- cinema-users-service/src/test/java/com/cinema/users/controller/UserControllerTest.java
+++ cinema-users-service/src/test/java/com/cinema/users/controller/UserControllerTest.java
@@ -2,16 +2,15 @@ package com.cinema.users.controller;
 
 import com.cinema.users.dto.*;
 import com.cinema.users.enums.Role;
-import com.cinema.users.mapper.UserMapper;
 import com.cinema.users.service.IUserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -39,7 +38,7 @@ class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private IUserService userService;
 
     private UserDTO testUserDTO;
